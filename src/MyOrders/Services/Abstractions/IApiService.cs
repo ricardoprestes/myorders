7aa0820156b3798e9bd29c13c158fd34843042ea
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-11-16 17:58:26 -0300

    Added ApiService

---
+++ src/MyOrders/Services/Abstractions/IApiService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyOrders.Models;
+
+namespace MyOrders.Services.Abstractions
+{
+    public interface IApiService
+    {
+        Task<List<Category>> GetCategories();
+        Task<List<Sale>> GetSales();
+        Task<List<Product>> GetProducts();
+    }
+}
---
+++ src/MyOrders/Services/ApiService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyOrders.Models;
+using MyOrders.Services.Abstractions;
+
+namespace MyOrders.Services
+{
+    public class ApiService : IApiService
+    {
+        public ApiService()
+        {
+        }
+
+        public Task<List<Category>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Sale>> GetSales()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
