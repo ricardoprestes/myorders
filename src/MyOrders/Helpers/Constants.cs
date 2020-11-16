3126c60ccd499dc44ebee1d744f91dcd62a6ac18
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-11-16 17:49:33 -0300

    Added url constants

---
+++ src/MyOrders/Helpers/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace MyOrders.Helpers
+{
+    public class Constants
+    {
+        public const string URL_CATEGORY = "https://pastebin.com/raw/YNR2rsWe";
+        public const string URL_PRODUCT = "https://pastebin.com/raw/eVqp7pfX";
+        public const string URL_SALE = "https://pastebin.com/raw/R9cJFBtG";
+    }
+}
