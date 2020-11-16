ffa38916cbe284c320722de2a1901d6fc8921a96
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-11-16 18:33:12 -0300

    Added IApiFunctions

--- src/MyOrders/Helpers/Constants.cs
+++ src/MyOrders/Helpers/Constants.cs
@@ -2,8 +2,9 @@
 {
     public class Constants
     {
-        public const string URL_CATEGORY = "https://pastebin.com/raw/YNR2rsWe";
-        public const string URL_PRODUCT = "https://pastebin.com/raw/eVqp7pfX";
-        public const string URL_SALE = "https://pastebin.com/raw/R9cJFBtG";
+        public const string URL_BASE = "https://pastebin.com/raw/";
+        public const string URL_CATEGORY = "YNR2rsWe";
+        public const string URL_PRODUCT = "eVqp7pfX";
+        public const string URL_SALE = "R9cJFBtG";
     }
 }
---
+++ src/MyOrders/Services/Abstractions/IApiFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyOrders.Helpers;
+using MyOrders.Models;
+using Refit;
+
+namespace MyOrders.Services.Abstractions
+{
+    [Headers("Content-Type: application/json")]
+    public interface IApiFunctions
+    {
+        [Get(Constants.URL_CATEGORY)]
+        Task<List<Category>> GetCategories();
+        [Get(Constants.URL_SALE)]
+        Task<List<Sale>> GetSales();
+        [Get(Constants.URL_PRODUCT)]
+        Task<List<Product>> GetProducts();
+    }
+}
