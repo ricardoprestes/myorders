ae77a3c76bcd890caf38eaae8d53b9d64f39d8bd
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-11-17 09:52:48 -0300

    Added FFImageLoading

--- src/MyOrders.Droid/Adapters/SaleProductAdapter.cs
+++ src/MyOrders.Droid/Adapters/SaleProductAdapter.cs
@@ -3,6 +3,7 @@ using Android.App;
 using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
+using FFImageLoading;
 using MyOrders.Droid.Helpers;
 using MyOrders.ViewModels;
 
@@ -36,8 +37,26 @@ namespace MyOrders.Droid.Adapters
         public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
         {
             var item = _viewModel.Items[position];
-            var myHolder = holder as SaleProductViewHolder;
-            myHolder.TxtHeader.Text = item.Type == Enums.EGroupItemType.Header ? item.Sale.Name : item.Product.Name;
+            var vh = holder as SaleProductViewHolder;
+            if(item.Type == Enums.EGroupItemType.Header)
+            {
+                vh.TxtHeader.Text = item.Sale.Name;
+                vh.TxtHeader.Visibility = ViewStates.Visible;
+                vh.CardItem.Visibility = ViewStates.Gone;
+            }
+            else
+            {
+                ImageService.Instance.LoadUrl(item.Product.Photo)
+                   .Retry(3, 200)
+                   .DownSample(100, 100)
+                   //.LoadingPlaceholder(Config.LoadingPlaceholderPath, FFImageLoading.Work.ImageSource.ApplicationBundle)
+                   //.ErrorPlaceholder(Config.ErrorPlaceholderPath, FFImageLoading.Work.ImageSource.ApplicationBundle)
+                   .Into(vh.ImgItem);
+
+                vh.TxtProductName.Text = item.Product.Name;
+                vh.CardItem.Visibility = ViewStates.Visible;
+                vh.TxtHeader.Visibility = ViewStates.Gone;
+            }
         }
 
         public override int ItemCount => _viewModel.Items.Count;
@@ -46,11 +65,18 @@ namespace MyOrders.Droid.Adapters
     public class SaleProductViewHolder : RecyclerView.ViewHolder
     {
         public TextView TxtHeader { get; set; }
+        public CardView CardItem { get; set; }
+        public TextView TxtProductName { get; set; }
+        public ImageView ImgItem { get; set; }
 
         public SaleProductViewHolder(View itemView, Action<RecyclerClickEventArgs> clickListener,
                             Action<RecyclerClickEventArgs> longClickListener) : base(itemView)
         {
             TxtHeader = itemView.FindViewById<TextView>(Resource.Id.txv_header);
+            CardItem = itemView.FindViewById<CardView>(Resource.Id.card_item);
+            TxtProductName = itemView.FindViewById<TextView>(Resource.Id.txv_product_name);
+            ImgItem = itemView.FindViewById<ImageView>(Resource.Id.img_item);
+
             itemView.Click += (sender, e) => clickListener(new RecyclerClickEventArgs { View = itemView, Position = AdapterPosition });
             itemView.LongClick += (sender, e) => longClickListener(new RecyclerClickEventArgs { View = itemView, Position = AdapterPosition });
         }
