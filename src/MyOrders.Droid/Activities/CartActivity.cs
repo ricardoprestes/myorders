640d5ffda7103e37a0edfe385f3d867bf32cd03b
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-11-18 15:18:47 -0300

    Show subtotal

--- src/MyOrders.Droid/Activities/CartActivity.cs
+++ src/MyOrders.Droid/Activities/CartActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Views;
+using Android.Widget;
 using MyOrders.ViewModels;
 
 namespace MyOrders.Droid.Activities
@@ -11,6 +12,8 @@ namespace MyOrders.Droid.Activities
         protected override int LayoutResource => Resource.Layout.activity_cart;
         public CartViewModel ViewModel { get; set; }
 
+        TextView _txvAmount, _txvTotalValue;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -19,6 +22,9 @@ namespace MyOrders.Droid.Activities
             Toolbar.Title = ViewModel.Title;
             SupportActionBar.SetDisplayHomeAsUpEnabled(true);
             SupportActionBar.SetHomeButtonEnabled(true);
+
+            _txvAmount = FindViewById<TextView>(Resource.Id.txv_amount);
+            _txvTotalValue = FindViewById<TextView>(Resource.Id.txv_total_value);
         }
 
         public override bool OnOptionsItemSelected(IMenuItem item)
@@ -31,6 +37,13 @@ namespace MyOrders.Droid.Activities
                     return true;
             }
             return base.OnOptionsItemSelected(item);
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            _txvAmount.Text = $"{App.Cart.Count} UN";
+            _txvTotalValue.Text = $"{App.Cart.Total:R$ ###,###,##0.00}";
         }
     }
 }
